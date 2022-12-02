3e4d03e537f41aa0181d23080dd067ebe21792e4
goktugocal <goktugocal41@gmail.com>
2022-12-02 15:01:28 +0300

    addresses

--- request.py
+++ request.py
@@ -1,9 +1,9 @@
 import requests
 
-# url = 'https://sha256-api.onrender.com:5000/messages'
-# myobj = "goktugocal"
+url = 'http://127.0.0.1:5000/messages'
+myobj = "goktugocal"
 
-# x = requests.post(url, json = myobj)
+x = requests.post(url, json = myobj)
 
 
 hashed = "e683c3e863b1718963a6ce705d8f8d821330540c3584b9d8d253cf2c6fe9d792"
